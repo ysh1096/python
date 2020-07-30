ab3c83dc0c4cdf654676d843c648ad8d3c70b2c4
Sunghwan Steven Yoo <ysh1096@gmail.com>
2020-07-30 06:55:45 -0800

    Add files via upload

---
+++ test_server.py
@@ -0,0 +1,17 @@
+#test_server.py
+
+import socket
+
+with socket.socket() as s:
+  s.bind(("0.0.0.0", 12345))
+  s.listen()
+  print("server is started")
+  conn, addr = s.accept()
+  
+  with conn:
+    print("connected by", addr)
+    while True:
+      data = conn.recv(1024)
+      if not data: break
+      conn.sendall(data)
+      
